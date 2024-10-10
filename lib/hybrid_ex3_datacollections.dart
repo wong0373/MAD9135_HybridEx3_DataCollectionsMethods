eb44b7da16f0335552c07044e8feaa91330f6f85
ShingKwan Terry Wong <wong0373@algonquinlive.com>
2024-10-10 16:09:25 -0400

    first commit

---
+++ lib/hybrid_ex3_datacollections.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+
+class Students {
+  List<Map<String, String>> people;
+
+  Students(this.people);
+}
---
+++ test/hybrid_ex3_datacollections_test.dart
@@ -0,0 +1,8 @@
+import 'package:hybrid_ex3_datacollections/hybrid_ex3_datacollections.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
